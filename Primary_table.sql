e2df29b98662ca956922218743f6b58a2b59348c
mludvik2 <mludvik2@yahoo.com>
2025-09-23 14:19:26 +0200

    Changed column positions

--- Primary_table.sql
+++ Primary_table.sql
@@ -2,14 +2,13 @@ SELECT *
 FROM czechia_price AS cp 
 WHERE date_part('year', cp.date_from) != date_part('year', cp.date_to)
 
-
+CREATE TABLE IF NOT EXISTS t_Michaeala_Papadimitriu_Ludvikova_project_SQL_primary_final AS 
 SELECT 
+	TO_char(cp.date_from, 'YYYY') AS YEAR,
+	cpib.name industry_branch,
 	cpc.name food_category,
 	cp.value food_price,
-	cpib.name industry_branch,
-	cpay.value avg_wage,
-	cp.region_code region_code,
-	TO_char(cp.date_from, 'DD. MM. YYYY') date_from
+	cpay.value avg_wage
 FROM czechia_price AS cp
 LEFT JOIN czechia_payroll AS cpay
 	ON date_part('year', cp.date_from) = cpay.payroll_year
@@ -18,7 +17,6 @@ LEFT JOIN czechia_payroll_industry_branch AS cpib
 LEFT JOIN czechia_price_category AS cpc 
 	ON cpc.code = cp.category_code
 WHERE cpay.value_type_code = 5958
-
 
 
 
