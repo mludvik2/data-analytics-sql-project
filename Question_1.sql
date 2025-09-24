dcbd733cdf771aca3c6e773a7c4dbfc58e8e0a8c
mludvik2 <mludvik2@yahoo.com>
2025-09-24 21:16:34 +0200

    added comparison of year by year comparison

--- Question_1.sql
+++ Question_1.sql
@@ -6,7 +6,7 @@ FROM t_michaeala_papadimitriu_ludvikova_project_sql_primary_final AS tmplpspf
 GROUP BY YEAR,industry_branch
 ORDER BY YEAR, industry_branch
 
-SELECT industry_branch,
+SELECT tmplpspf.industry_branch,
 	tmplpspf.YEAR,
 	tmplpspf2.year,
 	tmplpspf2.avg_wage - tmplpspf.avg_wage AS wage_difference
