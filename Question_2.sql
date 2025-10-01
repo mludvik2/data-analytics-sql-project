d347a833a94ac79ec51d0ebdbf85e1b7e42ae172
mludvik2 <mludvik2@yahoo.com>
2025-10-01 20:33:27 +0200

    formatting of 'end' placement

--- Question_2.sql
+++ Question_2.sql
@@ -7,24 +7,32 @@ FROM
 )
 SELECT
 	ROUND(MAX(CASE WHEN t.YEAR = t2.first_year AND food_category LIKE '%Mléko%' 
-                   THEN (avg_wage / avg_food_price)::NUMERIC END), 2) AS milk_2006,
+                   THEN (avg_wage / avg_food_price)::NUMERIC 
+                   END), 2) AS milk_2006,
 	ROUND(MAX(CASE WHEN t.YEAR = t2.last_year AND food_category LIKE '%Mléko%' 
-                   THEN (avg_wage / avg_food_price)::NUMERIC END), 2) AS milk_2018,
+                   THEN (avg_wage / avg_food_price)::NUMERIC 
+                   END), 2) AS milk_2018,
 	ROUND(
         (MAX(CASE WHEN t.YEAR = t2.last_year AND food_category LIKE '%Mléko%'
-                  THEN (avg_wage / avg_food_price)::NUMERIC END)
+                  THEN (avg_wage / avg_food_price)::NUMERIC 
+                  END)
        - MAX(CASE WHEN t.YEAR = t2.first_year AND food_category LIKE '%Mléko%'
-                  THEN (avg_wage / avg_food_price)::NUMERIC END)), 2
+                  THEN (avg_wage / avg_food_price)::NUMERIC 
+                  END)), 2
     ) AS milk_diff,
 	ROUND(MAX(CASE WHEN t.YEAR = t2.first_year AND food_category LIKE '%Chléb%'
-                   THEN (avg_wage / avg_food_price)::NUMERIC END), 2) AS bread_2006,
+                   THEN (avg_wage / avg_food_price)::NUMERIC 
+                   END), 2) AS bread_2006,
 	ROUND(MAX(CASE WHEN t.YEAR = t2.last_year AND food_category LIKE '%Chléb%'
-                   THEN (avg_wage / avg_food_price)::NUMERIC END), 2) AS bread_2018,
+                   THEN (avg_wage / avg_food_price)::NUMERIC 
+                   END), 2) AS bread_2018,
 	ROUND(
         (MAX(CASE WHEN t.YEAR = t2.last_year AND food_category LIKE '%Chléb%'
-                  THEN (avg_wage / avg_food_price)::NUMERIC END)
+                  THEN (avg_wage / avg_food_price)::NUMERIC 
+                  END)
        - MAX(CASE WHEN t.YEAR = t2.first_year AND food_category LIKE '%Chléb%'
-                  THEN (avg_wage / avg_food_price)::NUMERIC END)), 2
+                  THEN (avg_wage / avg_food_price)::NUMERIC 
+                  END)), 2
     ) AS bread_diff
 FROM
 	t_Michaeala_Papadimitriu_Ludvikova_project_SQL_primary_final AS t
