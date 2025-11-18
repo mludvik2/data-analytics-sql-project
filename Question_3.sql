00767a0b661c2e712b1f57ae0f4be7dac691db3e
mludvik2 <mludvik2@yahoo.com>
2025-11-18 12:53:59 +0100

    correction of tab from double to singular

--- Question_3.sql
+++ Question_3.sql
@@ -1,8 +1,8 @@
 WITH food_growth AS (
 SELECT 
-		food_category,
-		YEAR,
-		round(avg(avg_food_price)::NUMERIC, 2) AS price
+	food_category,
+	YEAR,
+	round(avg(avg_food_price)::NUMERIC, 2) AS price
 FROM
 	t_michaeala_papadimitriu_ludvikova_project_sql_primary_final AS tmplpspf
 GROUP BY
@@ -11,13 +11,13 @@ GROUP BY
 ),
 food_growth_calc AS (
 SELECT
-		food_category,
-		YEAR,
-		price,
-		LAG(price) OVER(PARTITION BY food_category ORDER BY YEAR) AS previous_price,
-		round(
-			((price - LAG(price) OVER (PARTITION BY food_category ORDER BY YEAR))/
-			LAG(price) OVER (PARTITION BY food_category ORDER BY YEAR ))* 100, 2) AS price_growth_perc
+	food_category,
+	YEAR,
+	price,
+	LAG(price) OVER(PARTITION BY food_category ORDER BY YEAR) AS previous_price,
+	round(
+		((price - LAG(price) OVER (PARTITION BY food_category ORDER BY YEAR))/
+		LAG(price) OVER (PARTITION BY food_category ORDER BY YEAR ))* 100, 2) AS price_growth_perc
 FROM
 	food_growth
 )
